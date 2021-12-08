5ed84134c95cc4dd359f1ee5feb4924115c52af4
crabboss950 <silverbell950@naver.com>
2021-12-08 13:26:34 +0900

    s

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
+    state = {
+        count:0,
+    };
+
+    handleIncrement = (event) => {
+        // staate 오브젝트 안에 있는 count를 증가 한뒤 state를 업데이트 해야함
+        this.setState({count: this.state.count + 1});
+    };
+
+    handleDecrement = (event) => {
+        // staate 오브젝트 안에 있는 count를 증가 한뒤 state를 업데이트 해야함
+        this.setState({count: this.state.count - 1});
+    };
+
     render() {
         return(
             <li className='habit'>
                 <span className='habit-name'>Reading</span>
-                <span className='habit-count'>8</span>
-                <button className='habit-button habit-increase'>
+                <span className='habit-count'>{this.state.count}</span>
+                <button className='habit-button habit-increase' onClick={this.handleIncrement}>
                 <i className="far fa-plus-square"></i>
                 </button>
-                <button className='habit-button habit-decrease'>
+                <button className='habit-button habit-decrease' onClick={this.handleDecrement}>
                 <i className="far fa-minus-square"></i>
                 </button>
                 <button className='habit-button habit-delete'>
