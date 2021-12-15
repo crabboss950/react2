1d2a371200576e455f8f1b664b13b94556b750a3
crabboss950 <silverbell950@naver.com>
2021-12-15 13:49:08 +0900

    오류 해결

--- src/app.jsx
+++ src/app.jsx
@@ -1,67 +1,75 @@
+import React,  { Component } from 'react';
 import './app.css';
 import Habits from './components/habits';
-import React, { Component } from 'react';
 import Navbar from './components/navbar';
-import HabitAddForm from './components/habitAddForm';
-
 
 class App extends Component {
+
   state = {
-    habits:[
-        { id:1, name:'Reading', count:0},
-        { id:2, name:'Running', count:0},
-        { id:3, name:'Coding', count:0},
+    habits: [
+      {id: 1,name:'Reading', count:0},
+      {id: 2,name:'Running', count:0},
+      {id: 3,name:'Coding', count:0},
     ],
-};
+  };
 
 handleIncrement = habit => {
-  // console.log(`handleIncrement ${habit.name}`);
-  const habits = this.state.habit.map(item => {
+  // const habits = [...this.state.habits];
+  // const index = habits.indexOf(habit);
+  // habits[index].count++;
+  const habits = this.state.habits.map(item => {
     if(item.id === habit.id){
-      return{...habit, count: habit.count + 1};
-     }
-     return item;
+      return {...habit,count:habit.count+1}
+    }
+    return item;
   });
   this.setState({habits});
-};
 
+};
 handleDecrement = habit => {
-  // console.log(`handleDelete ${habit.name}`);
-  const habits = this.state.habit.map(item => {
+  // const habits = [...this.state.habits];
+  // const index = habits.indexOf(habit);
+  // const count = habits[index].count -1;
+  // habits[index].count = count < 0 ? 0 : count;
+  const habits = this.state.habits.map(item => {
     if(item.id === habit.id){
-      const count = habit.count - 1;
-      return{...habit, count: count <0 ? 0:count};
-     }
-     return item;
+      const count = habit.count -1;
+      return {...habit,count:count<0 ? 0 : count}
+    }
+    return item;
   });
   this.setState({habits});
-};
 
+};
 handleDelete = habit => {
   const habits = this.state.habits.filter(item => item.id !== habit.id);
-  this.setState({habits});
+  this.setState({ habits});
 };
-
 handleAdd = name => {
-  const habits = [...this.state.habits, {id: Date.now(), name, count:0}];
+  const habits = [...this.state.habits, {id: Date.now(),name, count : 0}];
   this.setState({habits});
-};
-
-handleReset = () => {
-  const habits = this.state.habits.map(habit =>{
-    if(habit.count !==0){
-      return {...habit, count:0};
-    }
-    return habit;
-  });
-  this.setState({habits});
-};
-
-  render() {
-    return( 
-      <>
-        <Navbar totalCount={this.state.habits.filter(item => item.count > 0).length}/>
-        <Habits 
+}
+handleReset = () =>{
+        // const habits = this.state.habits.map(habit => {
+        //   habit.count = 0;
+        //   return habit;
+        // });
+        const habits = this.state.habits.map(habit => {
+          if(habit.count !== 0){
+            return {...habit,count:0}
+          }
+          return habit;
+        });
+        this.setState({habits});
+}
+render(){
+  // console.log()
+  return(
+    <>
+      <Navbar 
+        totalcount={this.state.habits.filter(item => item.count > 0).length}
+      />
+      <Habits 
         habits = {this.state.habits}
         onIncrement={this.handleIncrement}
         onDecrement={this.handleDecrement}
@@ -69,9 +77,12 @@ handleReset = () => {
         onAdd={this.handleAdd}
         onReset={this.handleReset}
       />
-      </>
+    </>
     );
   }
+
 }
+
+
 
 export default App;
